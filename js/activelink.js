89e1c7d3993182d088a348bfbb72d278816c08b2
Velliton <evg92faut@gmail.com>
2022-12-09 01:00:19 +0700

    links

--- js/activelink.js
+++ js/activelink.js
@@ -2,3 +2,8 @@ $(".header__menu__link").on("click", function() {
     $(".header__menu__link").removeClass("active");
     $(this).addClass("active");
 });
+
+$(".header__lang__item").on("click", function() {
+    $(".header__lang__item").removeClass("active");
+    $(this).addClass("active");
+});
